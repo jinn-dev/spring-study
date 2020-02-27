06f15dd3d3a0ab47c9f8bf097a6c452bce7ea665
jinn-dev <gsbyyy@naver.com>
2020-02-28 02:15:25 +0900

    -

--- src/main/java/first/common/controller/CommonController.java
+++ src/main/java/first/common/controller/CommonController.java
@@ -26,8 +26,10 @@ public class CommonController {
 	@RequestMapping(value = "/common/downloadFile.do")
 	public void downloadFile(CommandMap commandMap, HttpServletResponse response) throws Exception {
 		Map<String, Object> map = commonService.selectFileInfo(commandMap.getMap());
+		
 		String storedFileName = (String) map.get("STORED_FILE_NAME");
 		String originalFileName = (String) map.get("ORIGINAL_FILE_NAME");
+		
 		byte fileByte[] = FileUtils.readFileToByteArray(new File("C:\\03.dev\\file\\" + storedFileName));
 		
 		//읽어들인 파일 정보를 화면에서 다운로드 할 수 있도록 변환을 해주는 부분
@@ -36,6 +38,7 @@ public class CommonController {
 		response.setHeader("Content-Disposition",
 				"attachment; fileName=\"" + URLEncoder.encode(originalFileName, "UTF-8") + "\";");
 		response.setHeader("Content-Transfer-Encoding", "binary");
+		
 		response.getOutputStream().write(fileByte);
 		response.getOutputStream().flush();
 		response.getOutputStream().close();
